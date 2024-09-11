33472dafaefec5e253a265a7bb00bb667dcce101
Saffie-kth <safiatou@kth.se>
2024-09-11 11:40:11 +0200

    emg right arm

--- emg_right.py
+++ emg_right.py
@@ -1,2 +1,12 @@
 import pandas as pd                  #import pandas-libary to prosess data in tabells
 import matplotlib.pyplot as plt      #import matplotlib to create graphs
+
+# Read the data (via path)
+#file_path = "C:\\Användare\\Saffie\\Dokument\\emg_right.py"
+
+file_path = "C:\\Users\\DittNamn\\Documents\\filnamn.txt"
+
+# Öppna filen i läsläge ('r')
+with open(file_path, 'r') as file:
+    content = file.read()  # Läs innehållet i filen
+    print(content)  # Skriv ut filens innehåll till terminalen
