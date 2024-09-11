a8e05fd691d4df8f85f74aa1559dd70a1d1c7965
Saffie-kth <safiatou@kth.se>
2024-09-11 12:01:29 +0200

    emg right arm

--- emg_right.py
+++ emg_right.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt      #import matplotlib to create graphs
 # Read the data (via path)
 #file_path = "C:\\Användare\\Saffie\\Dokument\\emg_right.py"
 
-file_path = "C:\\Användare\\Saffie\\Dokument\\OpenSignals (r)evolution\\files\\punches\\opensignals_0007804b3c23_2024-09-09_15-04-53.txt"
-
+#file_path = "C:\\Användare\\Saffie\\Dokument\\OpenSignals (r)evolution\\files\\punches\\opensignals_0007804b3c23_2024-09-09_15-04-53.txt"
+file_path = "C:\\Skrivbord\\opensignals_0007804b3c23_2024-09-09_15-04-53.txt"
 # Öppna filen i läsläge ('r')
 with open(file_path, 'r') as file:
     content = file.read()  # Läs innehållet i filen
