60d2b4999d4bed463afc1a878c156ac7e30898a4
ellavillfor <ella.villfor@gmail.com>
2024-10-01 10:07:35 +0200

    cleaning

--- ella_test.py
+++
@@ -1,9 +0,0 @@
-# ella test to github
-print("hello world")
-x = 2
-if (x > 1):
-    print("x = ",x)
-    print("yey")
-    #test
-    #new test
-    # I think it's working
--- saffie_test.py
+++
@@ -1,7 +0,0 @@
-print("helloWorld")
-print("test")
-print("does it work?")
-print("wait, i think it works")
-#yay it finally works!!
-#or does it?
-#YES IT DOES!!!
