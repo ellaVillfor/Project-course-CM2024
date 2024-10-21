1415b4027afcf682b85b2eaa2994b537b368d28e
beatrixfigueiredo <beatrix.figueiredo12@gmail.com>
2024-10-21 13:58:12 +0200

    .

--- imu_functions.py
+++ imu_functions.py
@@ -3,9 +3,8 @@ import json
 import tkinter as tk
 from tkinter import filedialog
 import numpy as np
-from tabulate import tabulate
 import math
-from scipy.signal import butter, filtfilt, find_peaks
+from scipy.signal import find_peaks
 
 # Function to read the .json file
 def read_json_file():
@@ -48,6 +47,9 @@ def extract_acc_data(jsonDataAcc):
         originalTimestampsAcc[i] = (originalTimestampsAcc[i] - start_time) / 1000  # Convert to seconds
 
     # This function is more complex because the sensor collects more than one value per timestamp
+    # Because the  ovesense processes the data in batches - usually 8 values per timestamp
+    # This part of the function checks how many acc data we have for each timestamp and divides the interval between
+    # this and the next timestamp by the number of acc values
 
     # Process each pair of timestamps
     for i in range(len(originalTimestampsAcc) - 1):
@@ -74,14 +76,15 @@ def extract_acc_data(jsonDataAcc):
 
 # Function to calculate acceleration
 def calculate_acceleration(xAcc, yAcc, zAcc):
+    # acc is always going to be > 0 so it won't have in consideration the direction but it is okay because we only want 
+    # the peak value at the time the fist goes against the punching bag (we can see the acceleration coming almost to zero here)
     g = 9.81
     acc = [0] * len(xAcc)
     for i in range(len(xAcc)):
-        acc[i] = math.sqrt(pow(xAcc[i],2) + pow(yAcc[i],2) + pow(zAcc[i],2)) - g
-        # acc[i] = math.sqrt(pow((xAcc[i]-xAcc[i-1]),2) + pow((yAcc[i]-yAcc[i-1]),2) + pow((zAcc[i]-zAcc[i-1]),2))
+        acc[i] = math.sqrt(pow(xAcc[i],2) + pow(yAcc[i],2) + pow(zAcc[i],2)) - g 
     return acc
 
-## Punch detection and max acceleration finder
+# Punch detection and max acceleration finder
 def detect_punches_and_max_acc(timestamps, acc, window_size, calibration_time_threshold=1):
     # Ensure timestamps is a numpy array
     timestamps = np.array(timestamps)
@@ -93,9 +96,11 @@ def detect_punches_and_max_acc(timestamps, acc, window_size, calibration_time_th
     
     # Detect peaks using find_peaks
     punch_peaks, _ = find_peaks(filtered_acc, height=100, distance=100)  # Adjust height and distance in accordance to each dataset
+    # height = minimum height that a peak must have to be considered a valid peak
+    # distance = minimum distance between consecutive peaks for the frequency used (f=833Hz), distance = 100 = 1/833 * 100 = 0.12 sec
     
     max_accelerations = []
-    punch_intervals = []
+    # punch_intervals = []
     peak_timestamps = []
     
     for peak in punch_peaks:
@@ -108,7 +113,7 @@ def detect_punches_and_max_acc(timestamps, acc, window_size, calibration_time_th
         # Find max value within the punch segment
         max_acc = np.max(punch_segment)
         max_accelerations.append(max_acc)
-        punch_intervals.append((filtered_timestamps[peak], filtered_timestamps[end]))
+        # punch_intervals.append((filtered_timestamps[peak], filtered_timestamps[end]))
 
         # Get the exact timestamp of the peak acceleration
         peak_timestamp = filtered_timestamps[peak]
@@ -119,6 +124,7 @@ def detect_punches_and_max_acc(timestamps, acc, window_size, calibration_time_th
 
 # Function to plot the acceleration and detected punches
 def plot_punches_with_max_acc(timestamps, acc, punch_peaks, max_accelerations, peak_timestamps):
+    # This function plots a graph with the acceleration data and the peaks detected
     plt.figure(figsize=(12, 6))
     
     # Plot the combined acceleration
@@ -143,7 +149,10 @@ def plot_punches_with_max_acc(timestamps, acc, punch_peaks, max_accelerations, p
 
 # Function to adjust timestamps to start from when Z-acceleration exceeds threshold
 def adjust_timestamps_on_first_threshold(newTimestampsAcc, zAcc, threshold):
+    # This function is used to synchronize the time between the IMU and EMG data to make small check ups easier. 
+    # In the beggining of the procedure we make a small touch in the EMG sensors that causes a movement for the IMU
     # Find the index where Z-acceleration first exceeds the threshold
+    # we chose the z-axe because the movement is horizontal and in our movesense the vertical axe is the y
     for i, z in enumerate(zAcc):
         if z > threshold:
             threshold_index = i
@@ -159,18 +168,22 @@ def adjust_timestamps_on_first_threshold(newTimestampsAcc, zAcc, threshold):
 
 # Function to calculate speed by integrating acceleration over time
 def calculate_speed(acc, timestamps):
+    # This function would be used to calculate the speed using the trapezoidal method. 
+    # We will not be using it but we left it here because it might be helpful for further projects
+    # The sensor has an offset for each axis so to use this, we should put the acceleration back to zero before a punch
     # Initialize the speed list with the same shape as acceleration and starts with zero
     speed = np.zeros_like(acc)
 
     # Uses the trapezoidal integration method to calculate speed
     for i in range(1, len(acc)):
         speed[i] = speed [i-1] + np.trapz(acc[i-1:i+1], timestamps[i-1:i+1])
-        # speed[i] = np.trapz(acc[i-1:i+1], timestamps[i-1:i+1])
 
     return speed
 
 # Function to calculate position by integrating speed over time
 def calculate_position(speed, timestamps):
+    # This function would be used to calculate the position using the trapezoidal method. 
+    # We will not be using it but we left it here because it might be helpful for further projects
     # Initialize the position list with the same shape as speed and starts with zero
     position = np.zeros_like(speed)
 
@@ -181,19 +194,19 @@ def calculate_position(speed, timestamps):
 
     return position
 
-
-
-
 # Function to plot acceleration and speed
 def plot_acc_speed_and_position(timestamps, xAcc, yAcc, zAcc, xSpeed, ySpeed, zSpeed, xPosition, yPosition, zPosition, speed, position, acc):
+    # Antoher function that will not be used
+    # But what it does, it plots 3 graphs showing the acceleration, speed and position
+    # For each graph we have the data according to each axe and one for all of them together (yellow)
     # Create subplots for acceleration and speed
     plt.figure(figsize=(12, 12))
 
     # Plot the acceleration data
     plt.subplot(3, 1, 1)
-    # plt.plot(timestamps, xAcc, label='X Acceleration', color='r')
-    # plt.plot(timestamps, yAcc, label='Y Acceleration', color='g')
-    # plt.plot(timestamps, zAcc, label='Z Acceleration', color='b')
+    plt.plot(timestamps, xAcc, label='X Acceleration', color='r')
+    plt.plot(timestamps, yAcc, label='Y Acceleration', color='g')
+    plt.plot(timestamps, zAcc, label='Z Acceleration', color='b')
     plt.plot(timestamps, acc, label='Acceleration', color='y')
     plt.xlabel('Timestamps (seconds)')
     plt.ylabel('Acceleration (m/s^2)')
@@ -231,6 +244,3 @@ def plot_acc_speed_and_position(timestamps, xAcc, yAcc, zAcc, xSpeed, ySpeed, zS
     # Show the plot
     plt.tight_layout()
     plt.show()
-# # Plot the acceleration and speed data
-# # plot_acc_speed_and_position(adjustedTimestamps, xAcc, yAcc, zAcc, xSpeed, ySpeed, zSpeed, xPosition, yPosition, zPosition, speed)
-# plot_acc_speed_and_position(adjustedTimestamps, xAcc, yAcc, zAcc, xSpeed, ySpeed, zSpeed, xPosition, yPosition, zPosition, speed, position, acc)
